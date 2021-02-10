2bc306631cf0ef934111bc99d4179e5009e6ba31
Anton Panchenko <anton.panchenko@latoken.com>
2021-02-10 23:30:35 +0300

    Set measurements accuracy

--- client/utils/measure.py
+++ client/utils/measure.py
@@ -12,7 +12,6 @@ Usage:
 
 TODO: report every period
 TODO: batch reports
-TODO: set measurements accuracy
 TODO: optionally set label for any measured
 TODO: tests
 TODO: separate library
@@ -25,29 +24,32 @@ from functools import wraps
 from influxdb_client import InfluxDBClient, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-_bucket:  str
-_influx:  Any
-_delay:   int
-_period:  int = 0
-_records: Dict[str, Any] = {}
-_log      = logging.getLogger(__name__)
+_bucket:   str
+_influx:   Any
+_delay:    int
+_period:   int = 0
+_accuracy: str
+_records:  Dict[str, Any] = {}
+_log       = logging.getLogger(__name__)
 
 
-def init(url: str, org: str, token: str, bucket: str, delay_m: int = 1) -> None:
+def init(url: str, org: str, token: str, bucket: str, delay_m: int = 1, latency_accuracy: int = 6) -> None:
     """
     Initialize measures before any measured function called
 
-    url     : InfluxDB server API url
-    org     : organization name
-    token   : auth token
-    bucket  : destination bucket name
-    delay_m : minutes between consecutive reports
+    url              : InfluxDB server API url
+    org              : organization name
+    token            : auth token
+    bucket           : destination bucket name
+    delay_m          : minutes between consecutive reports
+    latency_accuracy : number of digits after point in latency
     """
-    global _influx, _bucket, _delay
+    global _influx, _bucket, _delay, _accuracy
     client = InfluxDBClient(url=url, org=org, token=token)
     _influx = client.write_api(write_options=SYNCHRONOUS)
     _bucket = bucket
     _delay = delay_m * 60
+    _accuracy = f'.{latency_accuracy}f'
     _log.info(f'Measure every {delay_m} minute' + ('s' if delay_m > 1 else ''))
 
 
@@ -57,7 +59,7 @@ class measured:
     """
     label: str
     count: int = 0
-    duration: float = 0
+    spent: float = 0
 
     def __call__(self, fn):
         self.label = fn.__name__
@@ -75,16 +77,16 @@ class measured:
             return f'{self.label} tps={0},latency={0} {ts}'
 
         tps = self.count / _delay
-        latency = self.duration / self.count
+        latency = self.spent / self.count
         self.count = 0
-        self.duration = 0
-        return f'{self.label} tps={tps},latency={latency} {ts}'
+        self.spent = 0
+        return f'{self.label} tps={tps:.3f},latency={format(latency, _accuracy)} {ts}'
 
 
 def _report(mon: measured, start: float) -> None:
     now = time.time()
     mon.count += 1
-    mon.duration += now - start
+    mon.spent += now - start
 
     global _records, _period
     _records[mon.label] = mon
@@ -93,10 +95,8 @@ def _report(mon: measured, start: float) -> None:
     if period <= _period:
         return
 
-    _period = period
-
     ts = str(period * _delay)
     records = [m.linear(ts) for m in _records.values()]
     _log.info(records)
-
+    _period = period
     _influx.write(bucket=_bucket, record=records, write_precision=WritePrecision.S)
